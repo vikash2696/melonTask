eacc7a9172cb17c42c29f35776e84d05dc8719d0
vikash2696 <vikash2696@gmail.com>
2018-10-22 10:13:29 +0530

    game logic completed

--- public/index.php
+++ public/index.php
@@ -8,7 +8,8 @@ use Zend\Stdlib\ArrayUtils;
  * to the application root now.
  */
 chdir(dirname(__DIR__));
-ini_set("display_errors",1);
+ini_set("display_errors",1); 
+// phpinfo(); die;
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,29 +1,56 @@
-function alphaOnly(event, char, id) {
-    var key;
-    if (window.event) {
-        key = window.event.key;
-    } else if (event) {
-        key = event.which;
-    }
-    var inputChar = String.fromCharCode(key);
-    var isAlpha = ((key >= 65 && key <= 90) || (key >= 95 && key <= 122));
-    if (isAlpha == true) {
-        validateCharacter(char, inputChar, id);
-    }
-    return isAlpha;
-}
-function validateCharacter(char, inputChar, id) {
-    var abc = document.getElementById("input_id_" + id);
-    if (char.toLowerCase() == inputChar.toLowerCase()) {
-        alert("matched");
-        abc.value = inputChar;
-        abc.classList.remove("error");
-        return true;
+var matchCount = 0;
+$(document).ready(function () {
+    wordLength = document.getElementById("word_length").value;
+    inputCount = new Array();
+});
+function alphaOnly(char, id) {
+    var inputField = document.getElementById("input_id_" + id);
+    var inputChar = inputField.value;
+    inputCount.push("input_id_" + id);
+    var isCharacter = isChar(inputChar);
+    if (isCharacter == false || isCharacter == null) {
+        inputField.classList.add("error");
+        inputField.value = "";
     } else {
-        alert("fail");
-        abc.classList.add("error");
-        abc.value = " ";
-        return 0;
+        if (char.toLowerCase() == inputChar.toLowerCase()) {
+            inputField.value = inputChar;
+            inputField.classList.remove("error");
+            matchCount++;
+            isWordComplete(matchCount, wordLength);
+            return true;
+        } else {
+            checkInputCount(inputCount);
+            inputField.classList.add("error");
+            inputField.value = "";
+        }
+    }
+
+}
+
+function checkInputCount(array_elements) {
+    var counts = {};
+    array_elements.forEach(function (x) {
+        counts[x] = (counts[x] || 0) + 1;
+    });
+    counts = JSON.stringify(counts);
+    for (cnt in counts) {
+        if(counts[cnt] == 5) {
+            alert("You lossed!!!");
+        }
+    }
+}
+function isChar(str) {
+    str = str.toLowerCase();
+    return str.match(/[a-z]/i);
+}
+
+function isWordComplete(matchCount, wordLength) {
+    if ((matchCount + 2) == wordLength) {
+        alert("You Won!!!");
+        matchCount = 0;
+        setTimeout(function () {
+            location.reload();
+        }, 1000);
     }
 
 }
